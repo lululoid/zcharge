d9bdf809cf051adcbca5d350ebae1184fe700810
lululoid <ereiwn@gmail.com>
2024-08-06 09:41:36 +0700

    Update: Use database for config

--- system/bin/zcharge.cpp
+++ system/bin/zcharge.cpp
@@ -3,11 +3,14 @@
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
+#include <sqlite3.h>
 #include <sstream>
 #include <string>
 #include <sys/types.h>
 #include <thread>
 #include <unistd.h>
+
+using namespace std;
 
 // Use directives to avoid repeatedly writing std::
 using std::cin;
@@ -30,6 +33,84 @@ void notif(const string &body) {
       "su -lp 2000 -c \"cmd notification post -S bigtext -t 'zcharge' 'Tag' '" +
       body + "'\"";
   system(cmd.c_str());
+}
+
+// Function to execute an SQL statement
+void execute_sql(sqlite3 *db, const string &sql) {
+  char *errmsg = 0;
+  int rc = sqlite3_exec(db, sql.c_str(), 0, 0, &errmsg);
+  if (rc != SQLITE_OK) {
+    cerr << "SQL error: " << errmsg << endl;
+    sqlite3_free(errmsg);
+  }
+}
+
+// Function to parse the configuration file and insert values into the database
+void parse_and_insert_config(sqlite3 *db, const string &config_file) {
+  ifstream infile(config_file);
+  string line;
+
+  // Prepare the SQL insert statement
+  string sql = "INSERT INTO zcharge_config (key, value) VALUES ";
+
+  while (getline(infile, line)) {
+    // Ignore comments and empty lines
+    if (line.empty() || line[0] == '#')
+      continue;
+
+    size_t pos = line.find('=');
+    if (pos != string::npos) {
+      string key = line.substr(0, pos);
+      string value = line.substr(pos + 1);
+
+      // Remove whitespace from key
+      key.erase(key.find_last_not_of(" \t\n\r\f\v") + 1);
+      key.erase(0, key.find_first_not_of(" \t\n\r\f\v"));
+
+      // Add the key-value pair to the SQL statement
+      sql += "('" + key + "', '" + value + "'),";
+    }
+  }
+
+  // Remove the last comma and add a semicolon
+  if (sql.back() == ',') {
+    sql.back() = ';';
+  } else {
+    sql += ';';
+  }
+
+  // Execute the SQL statement
+  execute_sql(db, sql);
+}
+
+// Function to convert a configuration file to a database
+void conf_to_db(const string &db_file, const string &config_file) {
+  sqlite3 *db;
+  int rc = sqlite3_open(db_file.c_str(), &db);
+  if (rc) {
+    cerr << "Can't open database: " << sqlite3_errmsg(db) << endl;
+    return;
+  } else {
+    cout << "Opened database successfully" << endl;
+  }
+
+  // Create the table for configuration parameters
+  string create_table_sql = R"(
+        CREATE TABLE IF NOT EXISTS zcharge_config (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            key TEXT NOT NULL,
+            value TEXT NOT NULL
+        );
+    )";
+  execute_sql(db, create_table_sql);
+
+  // Parse the configuration file and insert values into the database
+  parse_and_insert_config(db, config_file);
+
+  // Close the database
+  sqlite3_close(db);
+
+  cout << "Configuration inserted into the database successfully" << endl;
 }
 
 int read_bat_temp() {
